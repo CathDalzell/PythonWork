a44790e4166fe4749964ca13315c1f1dfc7bad6a
CathDalzell <catherine.j.dalzell@gmail.com>
2019-12-17 19:56:28 -0400

    An exercise to illustrate use of the .format method for printing strings.

---
+++ usingFormat.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+x = 4
+y = 5
+z = x + y
+z2 = x**2+ y**2
+print("The sum of x and y is {0:d}".format(z))
+
+print("The sum of squares of x and y is {0:d}".format(z2))
+
+print("")
+
+a = [1, 2, 3, 4]
+b = ["first", "second", "third", "fourth"]
+
+print("Output #2: {0}".format(a))
+print("Output #2: {0}".format(b))
+
+# print out a decimal
+
+w = 1.45
+print("Output #3, A decimal: {0:2.4f}".format(w))
