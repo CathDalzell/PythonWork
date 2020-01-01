e75257bcc332e43e5d0ba9321060c6951892d351
CathDalzell <catherine.j.dalzell@gmail.com>
2020-01-01 19:47:22 -0400

    More control statements.

--- controlStatements.py
+++ controlStatements.py
@@ -16,3 +16,19 @@ l[0] = 50
 print("Using a list")
 for i in l:
     print(i)
+
+print("Another example\n")
+m = list(range(0, 11))
+
+i = 0
+while i < 5:
+    print(m[i])
+    i += 1
+
+print("Using the break command\n")
+i = 0
+while True:
+    if i > 7:
+        break
+    print(m[i])
+    i += 1
